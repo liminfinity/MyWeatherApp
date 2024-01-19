2231174b2f9d164a7c68a7a60de88c152bac10ea
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-01-19 19:29:04 +0700

    init project

---
+++ Client/src/App.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react"
+
+export default function WeatherApp() {
+    return (
+        <StrictMode>
+            <textarea value='hi!'></textarea>
+        </StrictMode>
+        
+    )
+}
---
+++ Server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Started server on ${PORT} port`)
+})
+
